e623e26b1c4e05c6c33a2237e6358e2b58415988
Jernej Volk <volk.jernej@siol.net>
2023-07-27 09:54:55 +0200

    /

--- src/main.c
+++ src/main.c
@@ -3,26 +3,22 @@
 #include "uav_math/uav_math.h"
 
 int main() {
-	float lat = 46.397551f;
-	float lon = 15.633963f;
-	float alt = 270.0f;
+    struct Matrix vec1;
+    uav_matrix_init(&vec1, 3, 1);
+    struct Matrix vec2;
+    uav_matrix_init(&vec2, 3, 1);
 
-	float lat0 = 46.404747f;
-	float lon0 = 15.636317f;
-	float alt0 = 270.0f;
+    vec1.rows[0][0] = 1.0f;
+    vec1.rows[1][0] = 1.0f;
+    vec1.rows[2][0] = 0.0f;
 
-	float x, y, z;
-	float x_r, y_r, z_r;
-	float e, n, u;
+    vec2.rows[0][0] = 0.0f;
+    vec2.rows[1][0] = 1.0f;
+    vec2.rows[2][0] = 1.0f;
 
-	uav_trans_geodetic_to_ECEF(lat, lon, alt, &x, &y, &z);
-	uav_trans_geodetic_to_ECEF(lat0, lon0, alt0, &x_r, &y_r, &z_r);
+    float angle = uav_vec_angle_between(&vec1, &vec2);
 
-	uav_trans_ECEF_to_ENU(x, y, z, lat0, lon0, x_r, y_r, z_r, &e, &n, &u);
-
-	printf("%.3f %.3f %.3f\n", lat, lon, alt);
-	printf("%.3f %.3f %.3f\n", x, y, z);
-	printf("%.3f %.3f %.3f\n", e, n, u);
+	printf("%.3f\n", angle);
 
 	return 0;
 }
