b0fbe8d93989cc282b7d3a3e592a38f643674724
Jernej Volk <volk.jernej@siol.net>
2023-08-13 11:32:17 +0200

    /

--- src/main.c
+++ src/main.c
@@ -8,20 +8,20 @@ int main() {
 
     vec1.rows[0][0] = 0.0f;
     vec1.rows[1][0] = 0.0f;
-    vec1.rows[2][0] = -1.0f;
+    vec1.rows[2][0] = 1.0f;
 
     struct Matrix euler_angles;
     uav_matrix_init(&euler_angles, 3, 1);
 
-    euler_angles.rows[0][0] = 45.0f * 0.01745329f;
+    euler_angles.rows[0][0] = 0.0f * 0.01745329f;
     euler_angles.rows[1][0] = 45.0f * 0.01745329f;
     euler_angles.rows[2][0] = 0.0f;
 
     uav_rotation_body_to_inertial(&vec1, &euler_angles);
 
-    printf("%.3f\n", vec1.rows[0][0]);
-    printf("%.3f\n", vec1.rows[1][0]);
-    printf("%.3f\n", vec1.rows[2][0]);
+    printf("x: %.3f\n", vec1.rows[0][0]);
+    printf("y: %.3f\n", vec1.rows[1][0]);
+    printf("z: %.3f\n", vec1.rows[2][0]);
 
 	return 0;
 }
